c973e80e4f935398df368319592d612f47cf02af
heikki <hjzh578@gmail.com>
2017-11-24 09:30:20 +0800

    First commit

---
+++ Longest_Palindromic_Substring/Main.cpp
@@ -0,0 +1,20 @@
+//
+// Created by heikki on 17-11-24.
+//
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+public:
+    string longestPalindrome(string s)
+    {
+
+    }
+};
+
+int main()
+{
+
+}
